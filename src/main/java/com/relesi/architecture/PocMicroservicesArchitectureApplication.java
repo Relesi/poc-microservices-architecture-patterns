bf2a79cdc925270f84af54927080beb4adbdf302
relesi <renatolessa.2011@hotmail.com>
2021-08-15 18:03:59 -0300

    first commit to poc-patterns

---
+++ src/main/java/com/relesi/architecture/PocMicroservicesArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.relesi.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMicroservicesArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMicroservicesArchitectureApplication.class, args);
+	}
+
+}
