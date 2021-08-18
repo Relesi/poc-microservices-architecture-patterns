9f25d2670806f80c79a227f5ba2eab50b5364ddc
relesi <renatolessa.2011@hotmail.com>
2021-08-18 19:34:11 -0300

    implementatio validation return to request

---
+++ src/main/java/com/relesi/architecture/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,13 @@
+//package com.relesi.architecture.resources.exceptions;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//
+//import com.relesi.architecture.services.exceptions.ObjectNotFoundException;
+//
+//@ControllerAdvice
+//public class ResourceExceptionHandler {
+//	
+//	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, Http)
+//
+//}
--- src/main/java/com/relesi/architecture/services/CategoriaService.java
+++ src/main/java/com/relesi/architecture/services/CategoriaService.java
@@ -2,12 +2,12 @@ package com.relesi.architecture.services;
 
 import java.util.Optional;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.relesi.architecture.domain.Category;
 import com.relesi.architecture.repositories.CategoryRepository;
+import com.relesi.architecture.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,12 +17,13 @@ public class CategoriaService {
 
 	public Category search(Integer id) {
 		Optional<Category> obj = categoryRepository.findById(id);
+		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Object not found!  Id: " + id + ", Tipo: " + Category.class.getName(), null));
+				"Object not found!  Id: " + id + ", Tipo: " + Category.class.getName()));
 
 	}
 	
-	
+
 
 
 
