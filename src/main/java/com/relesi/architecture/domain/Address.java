0821c13d830f9002a9426ea660a8da71e7c8e486
relesi <renatolessa.2011@hotmail.com>
2021-08-24 14:30:02 -0300

    imple entitys

--- src/main/java/com/relesi/architecture/domain/Address.java
+++ src/main/java/com/relesi/architecture/domain/Address.java
@@ -10,5 +10,129 @@ public class Address {
 	private String zipCode;
 	
 	private Client client;
+	
+	public Address() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPublicArea() {
+		return publicArea;
+	}
+
+	public void setPublicArea(String publicArea) {
+		this.publicArea = publicArea;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + ((complement == null) ? 0 : complement.hashCode());
+		result = prime * result + ((district == null) ? 0 : district.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		result = prime * result + ((publicArea == null) ? 0 : publicArea.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (complement == null) {
+			if (other.complement != null)
+				return false;
+		} else if (!complement.equals(other.complement))
+			return false;
+		if (district == null) {
+			if (other.district != null)
+				return false;
+		} else if (!district.equals(other.district))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		if (publicArea == null) {
+			if (other.publicArea != null)
+				return false;
+		} else if (!publicArea.equals(other.publicArea))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+	
+	
+	
 
 }
--- src/main/java/com/relesi/architecture/domain/Client.java
+++ src/main/java/com/relesi/architecture/domain/Client.java
@@ -1,5 +1,10 @@
 package com.relesi.architecture.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import com.relesi.architecture.domain.enums.ClientType;
 
 public class Client {
@@ -9,6 +14,137 @@ public class Client {
 	private String email;
 	private Double itinOrEin;
 	private ClientType type;
+	
+	private List<Address> addresses = new ArrayList<>();
+	
+	private Set<String> phones = new HashSet<>();
+	
+	public Client() {
+		
+	}
+
+	public Client(Integer id, String name, String email, Double itinOrEin, ClientType type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.itinOrEin = itinOrEin;
+		this.type = type;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getItinOrEin() {
+		return itinOrEin;
+	}
+
+	public void setItinOrEin(Double itinOrEin) {
+		this.itinOrEin = itinOrEin;
+	}
+
+	public ClientType getType() {
+		return type;
+	}
+
+	public void setType(ClientType type) {
+		this.type = type;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Set<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		this.phones = phones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addresses == null) ? 0 : addresses.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itinOrEin == null) ? 0 : itinOrEin.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phones == null) ? 0 : phones.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (addresses == null) {
+			if (other.addresses != null)
+				return false;
+		} else if (!addresses.equals(other.addresses))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itinOrEin == null) {
+			if (other.itinOrEin != null)
+				return false;
+		} else if (!itinOrEin.equals(other.itinOrEin))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phones == null) {
+			if (other.phones != null)
+				return false;
+		} else if (!phones.equals(other.phones))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
 	
 	
 	
