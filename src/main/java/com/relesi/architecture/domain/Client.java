6eb07a00a857e783ebadc5f406e51dac2b17cc26
relesi <renatolessa.2011@hotmail.com>
2021-08-31 20:45:49 -0300

    update: using only @JsonIgnore instead @JsonBackReference

--- src/main/java/com/relesi/architecture/domain/Client.java
+++ src/main/java/com/relesi/architecture/domain/Client.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.relesi.architecture.domain.enums.ClientType;
 
 @Entity
@@ -31,7 +30,6 @@ public class Client implements Serializable {
 	private String itinOrEin;
 	private Integer type;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy = "client")
 	private List<Address> addresses = new ArrayList<>();
 
@@ -39,7 +37,7 @@ public class Client implements Serializable {
 	@CollectionTable(name = "TELEPHONE")
 	private Set<String> telephones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<PurchaseOrder> purchaseOrder = new ArrayList<>();
 
--- src/main/java/com/relesi/architecture/domain/Payment.java
+++ src/main/java/com/relesi/architecture/domain/Payment.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.relesi.architecture.domain.enums.PaymentState;
 
 @Entity
@@ -23,7 +23,7 @@ public abstract class Payment implements Serializable {
 	private Integer id;
 	private Integer state;
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "purchaseOrder_id")
 	@MapsId
