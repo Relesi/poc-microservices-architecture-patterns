9553a7f32b7f53871de66a3d7905b5284e1f7f43
relesi <renatolessa.2011@hotmail.com>
2021-09-18 06:13:37 -0300

    imnplement unittest to clientRepository

---
+++ src/test/java/com/relesi/architecture/repositories/ClienteRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.relesi.architecture.repositories;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.relesi.architecture.domain.Address;
+import com.relesi.architecture.domain.Client;
+import com.relesi.architecture.services.ClientService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class ClienteRepositoryTest {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private ClientService clientService;
+
+	public static final Integer ID = 1;
+
+	@Before
+	public void setUp() throws Exception {
+
+		Client client = new Client();
+		client.setName("Renato");
+		client.setEmail("renato@renato.com.br");
+		client.setItinOrEin("299.009.009.09");
+		// client.setType(null);
+
+		client.setAddresses(new ArrayList<Address>());
+		client.setTelephones(null);
+		client.setPurchaseOrder(new ArrayList<>());
+		this.clientRepository.save(client);
+
+	}
+
+	@Test
+	public void testSearch() {
+		final Optional<Client> client = Optional.ofNullable(this.clientService.search(ID));
+		assertTrue(client.isPresent());
+	}
+
+}
